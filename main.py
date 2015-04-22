a379a68ba3a90732357fa018c3ca284f92abbe1c
1egoman <rsg1egoman@gmail.com>
2015-04-22 10:19:17 -0400

    Added first main.py template

---
+++ main.py
@@ -0,0 +1,6 @@
+print "Let's print stuff!"
+print "Say your name, and something cool about yourself!"
+
+# each person should put a print statement for themselves
+# for example: print "Hi, I'm John, and I like pie."
+
