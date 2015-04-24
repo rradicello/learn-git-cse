00bd60342bd326ff514f7a9e2334dd7c19813d99
rradicello <11radrya647@BA-1432-DT-10.bville.org>
2015-04-24 10:26:30 -0400

    added Ryan Rad

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ print "Say your name, and something cool about yourself!"
 # each person should put a print statement for themselves
 # for example: print "Hi, I'm John, and I like pie."
 
+ print "HCHELLO!! Me am Ryan Radicello, 4 tiimes 6 is 48, divided by 2 is 24, divided by 3 is 4, to the power of 3 is 64"
